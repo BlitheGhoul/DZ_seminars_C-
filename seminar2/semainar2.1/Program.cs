682f8910168e27f4c30b9ccf14873014300754cf
Tosh <88.a.potapov@gmail.com>
2022-07-15 18:54:21 +0300

    add DZ seminar2

---
+++ seminar2/semainar2.1/Program.cs
@@ -0,0 +1,8 @@
+﻿//  Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает
+// вторую цифру этого числа.
+
+Console.Write("Введите трёхзначное число: ");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine("Ваше число: " + number);
+int number2 = (number / 10) % 10;
+Console.WriteLine("Вторая цифра Вашего числа: " + number2);
---
+++ seminar2/seminar 2.4/Program.cs
@@ -0,0 +1,36 @@
+﻿/* 
+Дополнительное задание: Напишите программу, которая принимает на вход число N и выводит все простые числа
+в промежутке от 0 до N (включительно). После, попробуйте поэксперментировать с величиной значения N и 
+понаблюдать за тем, как оно влияет на время выполнения программы.
+*/
+Console.Write("Введите число, вплоть до которого будут выводиться простые числа: ");
+int num = int.Parse(Console.ReadLine());
+int[] arr1 = new int[num];
+int size = num;
+int count = 0;
+while (count < size)
+{
+    arr1[count] = count + 1;
+    if (arr1[count] == 2)
+    {
+        Console.WriteLine(arr1[count]);
+    }
+    else if (arr1[count] == 3)
+    {
+        Console.WriteLine(arr1[count]);
+    }
+     else if (arr1[count] == 5)
+    {
+        Console.WriteLine(arr1[count]);
+    }
+     else if (arr1[count] == 7)
+    {
+        Console.WriteLine(arr1[count]);
+    }
+     else if (arr1[count] % 2 != 0 & arr1[count] % 3 != 0 & arr1[count] % 4 != 0 & arr1[count] % 5 != 0 &
+      arr1[count] % 6 != 0 & arr1[count] % 7 != 0 & arr1[count] % 8 != 0 & arr1[count] % 9 != 0 & arr1[count] != 1)
+    {
+        Console.WriteLine(arr1[count]);
+    }
+    count++;
+}
