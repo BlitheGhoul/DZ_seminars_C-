c7f9b0b6ad69aef4125c22f6166fa059169dc584
Tosh <88.a.potapov@gmail.com>
2022-08-07 22:08:20 +0300

    add seminar4

---
+++ seminar4/seminar4.3/Program.cs
@@ -0,0 +1,33 @@
+﻿/*Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+6, 1, 33 -> [6, 1, 33]
+*/
+
+void fillingArray(int[] a, int b, int c)
+{
+    for(int i = 0; i < a.Length; i++)
+    {
+        a[i] = new Random().Next(b,c);
+    }
+}
+void printArray(int[] a)
+{
+    for(int i = 0; i < a.Length; i++)
+    {
+        Console.Write(a[i] + " ");
+    }
+}
+
+Console.Write("Эта программа создаст  и выведет на экран массив из 8 случайных элементов, но ей нужно немного помочь. \nВведите минимальное число массива: ");
+int min = int.Parse(Console.ReadLine());
+Console.Write("\nОтлично!\n \nА теперь введите максимальное число массива: ");
+int max = int.Parse(Console.ReadLine()) + 1;
+
+int[] array = new int[8];
+fillingArray(array, min, max);
+Console.Write("\nВаш массив готов: [ ");
+printArray(array);
+Console.Write("]");
+
