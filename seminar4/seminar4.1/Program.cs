ce54f7181654514d39de2211bbeb3efc0fd9cb4e
Tosh <88.a.potapov@gmail.com>
2022-08-07 22:12:39 +0300

    TRYE add seminar4

---
+++ seminar4/seminar4.1/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+3, 5 -> 243 (3⁵)
+
+2, 4 -> 16
+*/
+
+Console.Write("Введите число, которое будет возводиться в степень: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.Write("\nВведите желаемую степень: ");
+int num2 = int.Parse(Console.ReadLine());
+int helpNumber = num1;
+
+for(int i = 1; i < num2; i++)
+{
+    num1 = num1 * helpNumber;
+}
+Console.WriteLine("\n Итоговое число: " + num1);
+
---
+++ seminar4/seminar4.2/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+452 -> 11
+
+82 -> 10
+
+9012 -> 12
+*/
+
+
+Console.Write("Введите число, для получения суммы его цифр: ");
+string preNumber = Console.ReadLine();
+int number = 0;
+for(int i = 0; i < preNumber.Length; i++)
+{
+  
+ number = number +  int.Parse(Convert.ToString(preNumber[i]));
+}
+Console.WriteLine("\nСумма цифр числа " + preNumber + " равна: " + number);
